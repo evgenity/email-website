b755bd7b70284841b5277fcda65e2649f7aeb93f
Evgeni <bikovevg@gmail.com>
2021-06-17 22:28:49 +0300

    create subscription after signup

--- server/email_website/website/views.py
+++ server/email_website/website/views.py
@@ -9,17 +9,34 @@ from django.shortcuts import get_object_or_404, render, redirect
 
 from django.contrib.auth import login, authenticate
 
+import uuid
+
+from subscriptions.models import Subscription
 
 # At this point, user is a User object that has already been saved
 # to the database. You can continue to change its attributes
 # if you want to change other fields.
+from datetime import datetime
 
 
 def home(request):
-    template = loader.get_template('website/index.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
+	try:
+		subscription = Subscription.objects.get(email=request.user.username)
+		api_key = subscription.api_key
+		print (api_key)
+	except Exception as e:
+		print (e)
+		api_key = None
+	template = loader.get_template('website/index.html')
+	context = {
+		'api_key' : api_key
+	}
+	return HttpResponse(template.render(context, request))
+
+def create_subscription(user):
+	subscription = Subscription(pub_date = datetime.now(), api_key = uuid.uuid4(), email = user.username)
+	subscription.save()
+
 
 def signup(request):
 	template = loader.get_template('website/signup.html')
@@ -33,17 +50,8 @@ def signup(request):
 			raw_password = form.cleaned_data.get('password1')
 			user = authenticate(username=username, password=raw_password)
 			login(request, user)
+			create_subscription(user)
 			return redirect('home')
-
-		# return HttpResponse(template.render(context, request))
-		
 	else:
 		form = UserCreationForm()
 	return render(request, 'website/signup.html', {'form': form}) 
-		# user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-		# user.save()
-	# return render(request, 'subscriptions/form.html', {
-	# 	'subscription': subscription,
-	# 	'post_data': post_data,
- #        'api_key': api_key
- #    })
