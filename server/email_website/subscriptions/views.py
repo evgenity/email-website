eef74620d4b35e0241d4650a6e7397ac5b307985
Evgeni <bikovevg@gmail.com>
2021-06-18 11:42:56 +0300

    email sender added

--- server/email_website/subscriptions/views.py
+++ server/email_website/subscriptions/views.py
@@ -20,6 +20,50 @@ def detail(request, subscription_id):
 	subscription = get_object_or_404(Subscription, pk=subscription_id)
 	return render(request, 'subscriptions/detail.html', {'subscription': subscription})
 
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+def send_email(receiver_email, msg):
+	port = 465  # For starttls
+	smtp_server = "smtp.gmail.com"
+	sender_email = "email.website.form@gmail.com"
+	password = 'qecmid-pagwan-jodmA4'
+	context = ssl.create_default_context()
+
+	message = MIMEMultipart("alternative")
+	message["Subject"] = "Form comleted"
+	message["From"] = sender_email
+	message["To"] = receiver_email
+
+	# Create the plain-text and HTML version of your message
+	text = """\
+	Hi,
+	How are you?"""
+
+	html = """\
+	<html>
+	  <body>
+	    <p>Hi,<br>
+	       {}
+	    </p>
+	  </body>
+	</html>
+	""".format(msg)
+	print(html)
+
+	# Turn these into plain/html MIMEText objects
+	part1 = MIMEText(text, "plain")
+	part2 = MIMEText(html, "html")
+
+	# Add HTML/plain-text parts to MIMEMultipart message
+	# The email client will try to render the last part first
+	message.attach(part1)
+	message.attach(part2)
+
+	with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, message.as_string())
+	pass
 
 def form(request, api_key):
 	try:
@@ -31,6 +75,7 @@ def form(request, api_key):
 		post_data.pop("csrfmiddlewaretoken", None)
 		form_data = FormData(subscription = subscription, form_data = str(post_data), send_date=datetime.now())
 		form_data.save()
+		send_email(receiver_email=subscription.email, msg=str(post_data))
 	else:
 		post_data = None
 	return render(request, 'subscriptions/form.html', {
